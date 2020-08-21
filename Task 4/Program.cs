3201412bb8b688e1e54f0a97770e60adecf1bd8d
Николай <semlya.kolyan@mail.ru>
2020-08-21 23:10:42 +0300

    Задача 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите уровень зарплаты");
+            int year, salary;
+            double result;
+            salary = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стаж");
+            year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Размер премии:");
+            if(year>0&year<=4)
+            {
+                result = salary * 0.10;
+                Console.WriteLine(result);
+            }
+            else if (year>4&year<=9)
+            {
+                result = salary * 0.15;
+                Console.WriteLine(result);
+            }
+            else if (year > 9 & year <= 14)
+            {
+                result = salary * 0.25;
+                Console.WriteLine(result);
+            }
+            else if (year > 14 & year <= 19)
+            {
+                result = salary * 0.35;
+                Console.WriteLine(result);
+            }
+            else if (year > 19 & year <= 24)
+            {
+                result = salary * 0.45;
+                Console.WriteLine(result);
+            }
+            else if (year > 24)
+            {
+                result = salary * 0.5;
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
