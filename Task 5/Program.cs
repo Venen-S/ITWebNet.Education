308f0338eab9d8f818afbd1a16eb2fb3bb834096
Николай <semlya.kolyan@mail.ru>
2020-08-22 14:42:20 +0300

    Задача 5

---
+++ Task 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long i, result = 1, num = 1;
+            Console.WriteLine("Введите число");
+            while (!long.TryParse(Console.ReadLine(), out i))
+                Console.WriteLine("Некорректный ввод данных");
+            Console.Write("Факториал {0} = ", i);
+            do
+            {
+                result *= num;
+                num++;
+            } while (num <= i);
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+}
